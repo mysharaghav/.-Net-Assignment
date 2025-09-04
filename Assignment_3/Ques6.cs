9942bbe0730755c43fbc5dffdae4a8e56cf732fa
utkarsh <utkarshraj309@gmail.com>
2025-09-04 16:16:53 +0530

    Assignment 3 & 4 Completed

---
+++ Assignment_3/Ques6.cs
@@ -0,0 +1,32 @@
+﻿// Q6: Create a partial class Employee with properties representing employee details.
+// In another part, implement methods for calculating salary based on different factors.
+
+using System;
+
+namespace Assignment_3
+{
+    internal class Ques6
+    {
+        partial class Employee
+        {
+            public string Name { get; set; }
+            public double BasicSalary { get; set; }
+            public double Allowance { get; set; }
+        }
+
+        partial class Employee
+        {
+            public double CalculateSalary()
+            {
+                return BasicSalary + Allowance;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee { Name = "Utkarsh", BasicSalary = 25000, Allowance = 5000 };
+            Console.WriteLine($"{emp.Name}'s Salary: {emp.CalculateSalary()}");
+            Console.WriteLine("Developed By Utkarsh Roll-48");
+        }
+    }
+}
