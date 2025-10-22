d858af37b5a242f33abe80427d80a6620d2efd3a
utkarsh <utkarshraj309@gmail.com>
2025-10-22 16:06:46 +0530

    MultiThreading Examples

---
+++ MultiThreading/Ques1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    internal class Ques1
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(PrintNumber);
+            t1.Start();
+            Thread t2 = new Thread(PrintChar);
+            t2.Start();
+            t1.Join();
+            t2.Join();
+        }
+        static void PrintNumber()
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.Write(i + " ");
+                Thread.Sleep(200);
+            }
+            Console.WriteLine();
+        }
+        static void PrintChar()
+        {
+            for(char c = 'A'; c <= 'J'; c++)
+            {
+                Console.Write(c + " ");
+                Thread.Sleep(300);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ MultiThreading/UniqueNum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MultiThreading
+{
+   
+        internal class UniqueNumberGenerator
+        {
+            static void Main(string[] args)
+            {
+                Random rand = new Random();
+                HashSet<int> numbers = new HashSet<int>();
+
+               
+                while (numbers.Count < 1000)
+                {
+                    int num = rand.Next(1, 100001);
+                    numbers.Add(num);
+                }
+
+                string filePath = "unique_numbers.csv";
+
+                
+                string csvLine = string.Join(",", numbers);
+
+                File.AppendAllText(filePath, csvLine + Environment.NewLine);
+
+                Console.WriteLine("1000 unique numbers generated and appended to CSV file successfully.");
+                Console.WriteLine("Developed By Utkarsh Roll-48");
+            }
+        }
+    }
+
+
